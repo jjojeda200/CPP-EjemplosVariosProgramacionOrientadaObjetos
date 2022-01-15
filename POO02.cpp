a289c7eec60691f498394fdf24d5ad9dc9129120
José Juan Ojeda Granados <jotafirebase@gmail.com>
2022-01-15 20:18:47 +0000

    Actualizados

--- POO02.cpp
+++ POO02.cpp
@@ -13,7 +13,7 @@ private:
     int variableI, variableII;
 
 public:
-    base() { printf("\e[0;33mConstructor base...\e[0m\n"); }
+    base() { printf("\e[0;33mConstructor base...\e[0m\n"); } // Constructor
     ~base() { printf("\e[0;33mDestructor base...\e[0m\n"); }
 
     void set(int a, int b)
@@ -23,7 +23,7 @@ public:
     }
     void mostrar()
     {
-        printf("Valores pasados al método set (clase base): %d, %d\n", variableI, variableII);
+        printf("Valores/atributos almacenados en el objeto (clase base): %d, %d\n", variableI, variableII);
     }
 };
 
@@ -33,11 +33,12 @@ private:
     int k;
 
 public:
-    derivada() { printf("\e[0;34mConstructor derivada...\e[0m\n\n"); }
-    ~derivada() { printf("\e[0;34mDestructor derivada...\e[0m\n\n"); }
+    derivada() { printf("\e[0;33mConstructor derivada...\e[0m\n"); } // Constructor
+    ~derivada() { printf("\e[0;33mDestructor derivada...\e[0m\n"); }
 
-    derivada(int x) { k = x; }
-    void mostrarDerivada() { printf("Valores pasados al método set (clase derivada): %d\n", k); }
+    derivada(int x) { k = x; } // Constructor que me permite invocar e inicializar el objeto a la vez
+
+    void mostrarDerivada() { printf("Valores/atributos almacenados en el objeto (clase derivada): %d\n", k); }
 };
 
 int main()
@@ -46,16 +47,15 @@ int main()
     
 
     // instaciación objeto clase base, comentada para seguimiento de ejecución de constructores
-    printf("\e[0;31mEjemlo Uno\n");
-    printf("\e[0;34mInstaciar objeto de la clase base invocando al metodo constructor:\n");
+    printf("\e[0;31mEjemplo Uno\e[0m\n");
+    printf("\e[0;34mInstaciar objeto de la clase base invocando al metodo constructor:\e[0m\n");
     base Variables0;
     Variables0.set(10, 20);
     Variables0.mostrar();
-    Variables0.~base();
-    printf("\e[0;34mLlamando explícitamente al destructor\n\n");
+    printf("\e[0m\n");
 
 
-    printf("\e[0;31mEjemlo Dos\n");
+    printf("\e[0;31mEjemplo Dos\n");
     // instaciación objeto clase derivada, primero se ejecuta el constructor de la clase más nativa
     // Al instaciar un objeto de la clase derivada, tambien se asignan las variable de la clase base,
     // que contendran valores al azar, los que existan en la zona de memoria que se asigan para guardar
@@ -63,22 +63,26 @@ int main()
     printf("\e[0;34mInstaciar objeto de la clase derivada sin invocar al metodo constructor:\n");
     printf("\e[0;37mderivada objeto0(30)\e[0m\n");
     derivada objeto0(30);
-    printf("\n\e[0;34mSe llama al método de la clase base: \e[0;37mobjeto0.mostrar();\e[0m\n");
+    printf("\e[0;34mSe llama al método de la clase base: \e[0;37mobjeto0.mostrar();\e[0m\n");
     objeto0.mostrar();
-    printf("\n\e[0;34mSe llama al método de la clase derivada: \e[0;37mobjeto0.mostrarDerivada();\e[0m\n");
-    objeto0.mostrarDerivada(); // acceder miembro de la clase derivada
+    printf("\e[0;34mSe llama al método de la clase derivada: \e[0;37mobjeto0.mostrarDerivada();\e[0m\n");
+    objeto0.mostrarDerivada();
+    printf("\e[0m\n");
 
-/*
-    derivada objeto1; // Instacia objeto de la clase derivada invoca al metodo constructor
+
+    printf("\e[0;31mEjemplo Tres\n");
+    printf("\e[0;34mInstaciar objeto de la clase derivada invocando al primer método constructor\n");
+    printf("sin pasar valor para la variable: \e[0;37mderivada objeto1\e[0m\n");
+    derivada objeto1;
+    printf("\e[0;34mSe pasan valores al objeto de la clase base invocando al metodo set:\n");
+    printf("\e[0;37mobjeto1.set(40, 50);\n");
     objeto1.set(40, 50);
+    printf("\e[0;34mSe llama al método de la clase base: \e[0;37mobjeto1.mostrar();\e[0m\n");
     objeto1.mostrar();
+    printf("\e[0;34mSe llama al método de la clase derivada: \e[0;37mobjeto1.mostrarDerivada();\e[0m\n");
+    objeto1.mostrarDerivada();
+    printf("\e[0m\n");
 
-    //derivada objeto1(30);
-
-    objeto1.set(10, 20); // acceder a miembro de base modificando los valores asignados anteriormente
-
-    objeto1.mostrar(); // acceder a miembro de la clase base
-    objeto1.mostrarDerivada(); // acceder miembro de la clase derivada
-*/
+    
     return 0;
 }
